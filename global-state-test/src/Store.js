86e0d9f7bfa84ea0e5b6092a7ea247aa537cc179
AlenaNik <alena.nik.digital@gmail.com>
2020-03-03 22:12:03 +0100

    store setup

---
+++ global-state-test/src/Store.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+const alenaState = {
+    name: 'Alena',
+    email: 'alena@gmail.com'
+}
+
+// creating a context
+
+export const Context = React.createContext();
+
+// wrapper component
+const Store = ({ children }) => {
+    const [state, setState] = useState(alenaState);
+    return (
+        <Context.Provider value={[state, setState]}>
+            {children}
+        </Context.Provider>
+    )
+}
