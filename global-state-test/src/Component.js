71e3b7bd975127942561c1b2ab6588054a066c6c
AlenaNik <alena.nik.digital@gmail.com>
2020-03-03 22:27:31 +0100

    fetching api

--- global-state-test/src/Component.js
+++ global-state-test/src/Component.js
@@ -4,12 +4,17 @@ import { Context } from './Store'
 export const Component = () => {
     const [state, setState] = useContext(Context)
 
+    const handleClick = async () => {
+        const person = await(await fetch('http://uinames.com/api/?ext')).json();
+        setState(person)
+    }
+
     return (
         <>
             <h2>I'm a component with context</h2>
             <p>name: {state.name}</p>
             <p>email: {state.email}</p>
-
+            <button onClick={handleClick}>Click me</button>
         </>
     )
 }
