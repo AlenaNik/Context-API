5de97242ce3ba74c20f67afba12ac0de9889d1b1
AlenaNik <alena.nik.digital@gmail.com>
2020-03-03 22:17:43 +0100

    grab values from a global state

--- global-state-test/src/Component.js
+++ global-state-test/src/Component.js
@@ -1,10 +1,15 @@
-import React from 'react'
-
+import React, {useContext} from 'react'
+import { Context } from './Store'
 
 export const Component = () => {
+    const [state, setState] = useContext(Context)
+
     return (
-        <div>
-            I'm from cool component
-        </div>
+        <>
+            <h2>I'm a component with context</h2>
+            <p>name: {state.name}</p>
+            <p>email: {state.email}</p>
+
+        </>
     )
 }
